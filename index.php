423e67de12847999656eec2e639703b22f3aa0d6
Kiran-mgh <66546755+Kiran-mgh@users.noreply.github.com>
2023-02-22 11:52:51 +0530

    Update index.php
    php-mailer

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'test@gmail.com';                     //SMTP username
-    $mail->Password   = 'pwd';                               //SMTP password
+    $mail->Password   = 'password';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
